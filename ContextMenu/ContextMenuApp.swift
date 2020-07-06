1d50a8d6bb1e52f681719c0cbbf0927a8955ae69
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-06 09:27:13 +0700

    Initial Commit

---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by Võ Thanh Sang on 7/6/20.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
